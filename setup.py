8aa4b21b6573b629d3a538dac812d0dc857709e7
growers12 <160653060+growers12@users.noreply.github.com>
2025-04-23 12:57:23 +0200

    Bump to 1.4.1, store version in a single place

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="configurator",
-    version="1.4.0",
+    version="1.4.1",
     description="System configuration scripts",
     long_description="System configuration scripts",
     author="HiFiBerry",
