b2e91c5686cf3d6fd8d68c62e38616d9c3625f5b
growers12 <160653060+growers12@users.noreply.github.com>
2025-07-01 13:54:50 +0200

    Removed installer

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
             "config-asoundconf=configurator.asoundconf:main",
             "config-configtxt=configurator.configtxt:main",
             "config-hattools=configurator.hattools:main",
-            "config-installer=configurator.installer:main",
             "config-detect=configurator.soundcard_detector:main",
             "config-detectpi=configurator.pimodel:main",
             "config-soundcard=configurator.soundcard:main",
