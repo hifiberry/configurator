172126d1f6aaf292d5ce4e190b7584c0b922ee8d
growers12 <160653060+growers12@users.noreply.github.com>
2025-07-01 11:22:55 +0200

    Add missing dependencies

--- build-deb.sh
+++ build-deb.sh
@@ -3,5 +3,19 @@ set -e
 
 cd `dirname $0`
 
-sbuild --chroot-mode=unshare --no-clean-source
+# Check if DIST is set by environment variable
+if [ -n "$DIST" ]; then
+    echo "Using distribution from DIST environment variable: $DIST"
+    DIST_ARG="--dist=$DIST"
+else
+    echo "No DIST environment variable set, using sbuild default"
+    DIST_ARG=""
+fi
+
+sbuild \
+    --chroot-mode=unshare \
+    --no-clean-source \
+    --enable-network \
+    $DIST_ARG \
+    --verbose
 
