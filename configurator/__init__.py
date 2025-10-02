4cceecc365d93ccea1ca3027843212c03f867955
HiFiBerry <info@hifiberry.com>
2025-10-02 10:17:18 +0100

    feat: Bump version to 2.5.1 and update changelog with new features including headphone volume control and memory information enhancements

--- configurator/__init__.py
+++ configurator/__init__.py
@@ -4,6 +4,6 @@ HiFiBerry Configurator Package
 System configuration scripts for HiFiBerry devices.
 """
 
-__version__ = "1.6.7"
+__version__ = "2.5.1"
 __author__ = "HiFiBerry"
 __email__ = "support@hifiberry.com"
