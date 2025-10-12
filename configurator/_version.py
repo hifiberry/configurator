c8530a5dd88ecfdd91cd29b223d383fc637e9069
HiFiBerry <info@hifiberry.com>
2025-10-12 08:14:38 +0100

    feat: Update version to 2.6.1 and add --detected option for sound card detection

--- configurator/_version.py
+++ configurator/_version.py
@@ -4,7 +4,7 @@ Version information for HiFiBerry Configurator
 Single source of truth for version number
 """
 
-__version__ = "2.6.0"
+__version__ = "2.6.1"
 __version_info__ = tuple(map(int, __version__.split('.')))
 
 # For backward compatibility
