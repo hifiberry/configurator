7875edf15461efbca99ac66b0b3f93ed54b5c829
HiFiBerry <info@hifiberry.com>
2026-02-06 09:38:13 +0000

    Set force_eeprom_read=0 when configuring fixed sound card
    - Prevents EEPROM detection from overriding fixed configuration
    - Version 2.12.14

--- configurator/handlers/soundcard_handler.py
+++ configurator/handlers/soundcard_handler.py
@@ -274,6 +274,7 @@ class SoundcardHandler:
                 # Remove existing HiFiBerry overlays and set the new one
                 config.remove_hifiberry_overlays()
                 config.disable_detection()
+                config.disable_eeprom()
                 config.enable_overlay(dtoverlay, card_name=card_name)
                 config.save()
                 
